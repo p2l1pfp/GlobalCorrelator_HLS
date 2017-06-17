68c7b7a4e40bd3a8c1eeab2cf90f1e9a96362c57
Philip Coleman Harris <pharris@dtmit07.cern.ch>
2017-06-17 04:30:43 +0200

    Added a simple vertexing, just as a example to understand HLS. Latency is roughly 20 clocks.

---
+++ simple_vtx_ref.cpp
@@ -0,0 +1,37 @@
+#include "src/data.h"
+#include "src/simple_vtx.h"
+#include <cmath>
+#include <cassert>
+#ifndef __SYNTHESIS__
+#include <cstdio>
+#endif
+
+void simple_vtx_ref(TkObj track[NALLTRACK], VtxObj *outvtx) { 
+  //Fill sum Et and count binned in dZ
+  ap_int<5>  vtxbin[NVTXBINS];
+  pt_t       sumbin[NVTXBINS];
+  for (int ivtx = 0; ivtx < NVTXBINS; ++ivtx) { vtxbin[ivtx] = 0;  sumbin[ivtx] = 0;}
+  //  for(int isec=0; isec < NSECTOR;    isec++) { 
+  for(int it=0; it < NALLTRACK; it++) { 
+    ap_int<5> bin=((track[it].hwZ0))*1./Z0_SCALE*(NVTXBINS/15);
+    if(bin < 0)        bin = 0;
+    if(bin > NVTXBINS) bin = NVTXBINS;
+    pt_t       pt =track[it].hwPt;
+    pt > VTXPTMAX ? pt=VTXPTMAX : pt=pt;
+    vtxbin[bin]++;
+    sumbin[bin]+=pt;
+  }
+  //Loop through bins and find maximum
+  outvtx->hwSumPt = 0;
+  outvtx->mult    = 0;
+  outvtx->hwZ0    = 0;
+  for(int it=0; it < NVTXBINS; it++) { 
+    if(sumbin[it] > outvtx->hwSumPt) { 
+      outvtx->hwSumPt = sumbin[it];
+      outvtx->mult    = vtxbin[it];
+      outvtx->hwZ0    = it*Z0_SCALE*(NVTXBINS/15)+Z0_SCALE*0.5;
+    }
+  }
+  outvtx->mult > 3 ? outvtx->hwId = 1 : outvtx->hwId = 0;
+}
+
---
+++ simple_vtx_test.cpp
@@ -0,0 +1,51 @@
+#include <cstdio>
+#include "src/simple_vtx.h"
+
+#define NTEST 50
+#define NPV   15
+
+int main() {
+  srand(73); // 73 is also a prime number just to be different than Gio ;p
+  float zPVSig = 0.7; 
+  float zPU    = 4.; 
+  int   nSubSector = NALLTRACK/NSECTOR;
+  int   nSubPV     = NPV      /NSECTOR;
+  TkObj  track[NSECTOR][NALLTRACK]; 
+  VtxObj outvtx; VtxObj outvtxref;
+  for (int test = 1; test <= NTEST; ++test) {
+    float  gaus = float(rand())/RAND_MAX+float(rand())/RAND_MAX;
+    float  zPV  = gaus*zPU;
+    for(int j  = 0; j < NSECTOR; j++) { 
+      int npucharged = (rand() % (nSubSector-nSubPV)/2) + (nSubSector-nSubPV)/2;
+      int npvcharged = (rand() % nSubPV/2             ) + nSubPV/2;
+      for (int i = 0; i < nSubSector; ++i) {
+	track[j][i].hwPt = 0; track[j][i].hwPtErr = 0; track[j][i].hwEta = 0; track[j][i].hwPhi = 0; track[j][i].hwZ0 = 0;
+      }
+      for(int i  = 0; i < npucharged; i++) { 
+	float pt = 20*(rand()/float(RAND_MAX))+2, eta = (rand()/float(RAND_MAX))*2.0-1.0, phi = (rand()/float(RAND_MAX))*2.0-1.0;
+	float gaus = float(rand())/float(RAND_MAX)+float(rand())/float(RAND_MAX);
+	float z  = gaus*zPU;
+	track[j][i].hwPt    = pt * PT_SCALE;
+	track[j][i].hwPtErr = (0.2*pt+4) * PT_SCALE; 
+	track[j][i].hwEta = eta * ETAPHI_SCALE;
+	track[j][i].hwPhi = phi * ETAPHI_SCALE;
+	track[j][i].hwZ0  = z * Z0_SCALE;
+      }
+      for(int i  = npucharged; i < npucharged+npvcharged; i++) { 
+	float pt = 100*(rand()/float(RAND_MAX))+2, eta = (rand()/float(RAND_MAX))*2.0-1.0, phi = (rand()/float(RAND_MAX))*2.0-1.0;
+	float  pGaus = 0.5*(rand()/RAND_MAX+rand()/RAND_MAX);
+	float  pZPV  = (pGaus-0.5)*zPVSig+zPV;
+	track[j][i].hwPt    = pt * PT_SCALE;
+	track[j][i].hwPtErr = (0.2*pt+4) * PT_SCALE; 
+	track[j][i].hwEta = eta * ETAPHI_SCALE;
+	track[j][i].hwPhi = phi * ETAPHI_SCALE;
+	track[j][i].hwZ0  = pZPV * Z0_SCALE;
+      }
+    }
+    //simple_vtx_hwopt(track[0],track[1],track[2],track[3],track[4],track[5], &outvtx);
+    simple_vtx_hwopt(track[0], &outvtx);
+    simple_vtx_ref  (track[0], &outvtxref);
+    std::cout << "====>  hardware dZ:" << float(outvtx.hwZ0) << " -- emulated dZ: " << float(outvtxref.hwZ0) << " -- True dZ: " << zPV*Z0_SCALE << std::endl;
+  }
+  return 0;
+}
