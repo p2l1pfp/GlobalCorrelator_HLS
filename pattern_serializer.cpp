d4d6b1eeddca7a14d2af0cf978cd4f2386def1a1
Giovanni <gpetruc@gmail.com>
2017-09-12 13:14:12 +0200

    Support for muxing in patterns

--- pattern_serializer.cpp
+++ pattern_serializer.cpp
@@ -1,7 +1,8 @@
 #include "pattern_serializer.h"
+#include <cassert>
 
-MP7PatternSerializer::MP7PatternSerializer(const std::string &fname, const std::string &boardName) :
-    fname_(fname), file_(nullptr), ipattern_(0) 
+MP7PatternSerializer::MP7PatternSerializer(const std::string &fname, unsigned int nmux, const std::string &boardName) :
+    fname_(fname), nmux_(nmux), nchann_(MP7_NCHANN/nmux), file_(nullptr), ipattern_(0) 
 {
     if (!fname.empty()) {
         file_ = fopen(fname.c_str(), "w");
@@ -9,11 +10,17 @@ MP7PatternSerializer::MP7PatternSerializer(const std::string &fname, const std::
         fprintf(file_, " Quad/Chan :    q00c0      q00c1      q00c2      q00c3      q01c0      q01c1      q01c2      q01c3      q02c0      q02c1      q02c2      q02c3      q03c0      q03c1      q03c2      q03c3      q04c0      q04c1      q04c2      q04c3      q05c0      q05c1      q05c2      q05c3      q06c0      q06c1      q06c2      q06c3      q07c0      q07c1      q07c2      q07c3      q08c0      q08c1      q08c2      q08c3      q09c0      q09c1      q09c2      q09c3      q10c0      q10c1      q10c2      q10c3      q11c0      q11c1      q11c2      q11c3      q12c0      q12c1      q12c2      q12c3      q13c0      q13c1      q13c2      q13c3      q14c0      q14c1      q14c2      q14c3      q15c0      q15c1      q15c2      q15c3      q16c0      q16c1      q16c2      q16c3      q17c0      q17c1      q17c2      q17c3  \n");
         fprintf(file_, "      Link :     00         01         02         03         04         05         06         07         08         09         10         11         12         13         14         15         16         17         18         19         20         21         22         23         24         25         26         27         28         29         30         31         32         33         34         35         36         37         38         39         40         41         42         43         44         45         46         47         48         49         50         51         52         53         54         55         56         57         58         59         60         61         62         63         64         65         66         67         68         69         70         71    \n");
     }
+    if (nmux_ > 1) {
+        assert(MP7_NCHANN % nmux_ == 0);
+        buffer_.resize(nmux_);
+        zero();
+    }
 }
 
 MP7PatternSerializer::~MP7PatternSerializer() 
 {
     if (file_) {
+        if (nmux_ > 1 && (ipattern_ % nmux_ != 0)) flush();
         fclose(file_); file_ = nullptr;
         printf("Saved %u MP7 patterns to %s.\n", ipattern_, fname_.c_str());
     }
@@ -22,12 +29,41 @@ MP7PatternSerializer::~MP7PatternSerializer()
 void MP7PatternSerializer::operator()(const MP7DataWord event[MP7_NCHANN]) 
 {
     if (!file_) return;
-    fprintf(file_, "Frame %04u :", ipattern_);
+    if (nmux_ == 1) print(ipattern_, event);
+    else push(event);
+    ipattern_++;
+}
+template<typename T> void MP7PatternSerializer::print(unsigned int iframe, const T & event) 
+//void MP7PatternSerializer::print(const MP7DataWord event[MP7_NCHANN]) 
+{
+    fprintf(file_, "Frame %04u :", iframe);
     for (unsigned int i = 0; i < MP7_NCHANN; ++i) {
         fprintf(file_, " 1v%08x", unsigned(event[i]));
     }
     fprintf(file_, "\n");
-    ipattern_++;
+}
+void MP7PatternSerializer::push(const MP7DataWord event[MP7_NCHANN])
+{
+    int imux = (ipattern_ % nmux_), offs = imux * nchann_;
+    for (unsigned int im = 0, i = 0; im < nmux_; ++im) {
+        for (unsigned int ic = 0; ic < nchann_; ++ic, ++i) {
+            buffer_[im][offs+ic] = event[i];
+        }
+    }
+    if (imux == nmux_-1) flush();
+}
+void MP7PatternSerializer::flush() {
+    for (unsigned int im = 0, iframe = ipattern_ - nmux_ + 1; im < nmux_; ++im, ++iframe) {
+        print(iframe, buffer_[im]);
+    }
+    zero();
+}
+void MP7PatternSerializer::zero() {
+    for (unsigned int i = 0; i < nmux_; ++i) {
+        for (unsigned int j = 0; j < MP7_NCHANN; ++j) {
+            buffer_[i][j] = 0;
+        }
+    }
 }
 
 HumanReadablePatternSerializer::HumanReadablePatternSerializer(const std::string &fname) :
--- pattern_serializer.h
+++ pattern_serializer.h
@@ -1,17 +1,46 @@
 #include "src/data.h"
 #include <cstdio>
+#include <vector>
 
 class MP7PatternSerializer {
     public:
-        MP7PatternSerializer(const std::string &fname, const std::string &boardName = "Board MP7_L1PF") ;
+        /** 
+            if nmux > 1, we assume that each event is sent in nmux clock cycles using nchann = MP7_NCHANN/nmux channels.
+            there is no time offsetting in the inputs, i.e. the first nmux events all start at clock 0 end end at clock nmux-1
+              at clock = 0,      channel[ 0      ...   nchann-1 ] = first nchann words of region (or event) 1
+              at clock = 0,      channel[ nchann ... 2*nchann-1 ] = first nchann words of region (or event) 2
+              at clock = 0,      channel .....
+              at clock = 1,      channel[ 0      ...   nchann-1 ] = second nchann words of region (or event) 1
+              at clock = 1,      channel[ nchann ... 2*nchann-1 ] = second nchann words of region (or event) 2
+              at clock = 1,      channel .....
+              at clock = nmux-1, channel[ 0      ...   nchann-1 ] = last nchann words of region (or event) 1
+        */
+        MP7PatternSerializer(const std::string &fname, unsigned int nmux=1, const std::string &boardName = "Board MP7_L1PF") ;
         ~MP7PatternSerializer() ;
         
         void operator()(const MP7DataWord event[MP7_NCHANN]) ;
-
+        
     protected:
         const std::string fname_;
+        const unsigned int nmux_, nchann_;
         FILE *file_;
         unsigned int ipattern_;
+        class Pattern {
+            public:
+                Pattern() {}
+                Pattern(const Pattern & other) { for (unsigned int i = 0; i < MP7_NCHANN; ++i) words[i] = other.words[i]; }
+                MP7DataWord & operator[](int i) { return words[i]; }
+                const MP7DataWord & operator[](int i) const { return words[i]; }
+                void operator=(const Pattern & other) { for (unsigned int i = 0; i < MP7_NCHANN; ++i) words[i] = other.words[i]; }
+            private:
+                MP7DataWord words[MP7_NCHANN];
+        };
+        std::vector<Pattern> buffer_; // for muxing; holds the next patterns in output format. will fill nmux events, first, then print them all out
+
+        template<typename T> void print(unsigned int iframe, const T & event);
+        void push(const MP7DataWord event[MP7_NCHANN]);
+        void flush();
+        void zero();
     
 };
 
