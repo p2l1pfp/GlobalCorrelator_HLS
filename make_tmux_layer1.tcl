a1510a856a453459f7096c9084f85646cb799d48
Dylan R <dylansr90@gmail.com>
2019-08-07 15:36:23 -0500

    file location fix

--- make_tmux_layer1.tcl
+++ make_tmux_layer1.tcl
@@ -16,7 +16,7 @@ add_files -tb utils/DiscretePFInputs_IO.h -cflags "-DTESTMP7 -std=c++0x"
 #add_files -tb data/regions_TTbar_PU140.dump
 #add_files -tb data/barrel_sectors_1x1_TTbar_PU140.dump
 add_files -tb data/barrel_sectors_1x1_TTbar_PU200.dump
-add_files -tb dummy.dump
+add_files -tb data/dummy.dump
 add_files -tb puppi/simple_puppi_ref.cpp -cflags "-DTESTMP7"
 add_files -tb vertexing/simple_vtx_ref.cpp -cflags "-DTESTMP7"
 
