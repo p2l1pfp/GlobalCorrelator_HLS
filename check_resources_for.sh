6a5d5c6ea9ecc06bca8790d66cc9677d78462cc3
Dylan R <dylansr90@gmail.com>
2019-08-07 14:42:30 -0500

    adding barebones scripts

---
+++ check_resources_for.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+mkdir -p reports_forward
+
+#clock=(5.000 4.167 3.571 3.125 2.500)
+#clock_freq=(200.000 240.000 280.000 320.000 400.000)
+
+#clock=(5.000 4.167 3.571 3.125)
+#clock_freq=(200.000 240.000 280.000 320.000)
+
+#clock=(5.000 3.571 3.125)
+#clock_freq=(200.000 280.000 320.000)
+
+#clock=(5.000 3.571)
+#clock_freq=(200.000 280.000)
+
+clock=(4.167)
+clock_freq=(240.000)
+
+#clock=(3.3333)
+#clock_freq=(300.000)
+
+for ncalo in 20 30 40
+do
+  for ii in 2
+  do
+    for iclk in 0
+    do
+      #for part in 'xcku115-flvb2104-2-i'
+      for part in 'xcvu9p-flgb2104-2-i'
+      do
+        clockname=`echo "${clock_freq[iclk]}" | sed -r 's/\..*//g'`
+        partname=`echo "$part" | sed -r 's/-.*//g'`
+        echo $clockname
+        echo $partname
+        ./run_barebones_for.sh ${ncalo} ${ii} ${clock[iclk]} ${clock_freq[iclk]} ${part}
+        cp l1pf-resource-test-forward/solution/syn/report/mp7wrapped_pfalgo3_forward_csynth.rpt reports_forward/mp7wrapped_pfalgo3_forward_csynth_ncalo${ncalo}_ii${ii}_clock${clockname}_${partname}.rpt
+        cp pftest_tieoff/pftest_tieoff.runs/impl_1/design_1_wrapper_power_routed.rpt reports_forward/design_1_wrapper_power_routed_ncalo${ncalo}_ii${ii}_clock${clockname}_${partname}.rpt
+      done
+    done
+  done
+done
---
+++ run_barebones.sh
@@ -0,0 +1,118 @@
+#!/bin/sh
+
+PF_DIR=/data/drankin/GlobalCorrelator_HLS/
+
+echo -e `date +"%h%y %T"` "BB [${LINENO}]  \t "
+
+if [[ "$1x" == "x" ]]; then
+    echo -e `date +"%h%y %T"` "BB [${LINENO}]  \t usage: ./run_barebones.sh <NTRACKS> <NCALOS> <II> <CLOCK_PERIOD> <CLOCK_FREQ> <PARTID>" 
+    exit
+fi
+
+SCRIPT_DIR="$( 
+  cd "$(dirname "$(readlink "$0" || printf %s "$0")")"
+  pwd -P 
+)"
+
+
+echo -e `date +"%h%y %T"` "BB [${LINENO}]  \t Script Path = $SCRIPT_DIR"
+
+NTRACKS=$1
+echo -e `date +"%h%y %T"` "BB [${LINENO}]  \t NTRACKS = $NTRACKS"
+NCALOS=$2
+echo -e `date +"%h%y %T"` "BB [${LINENO}]  \t NCALOS = $NCALOS"
+II=$3
+echo -e `date +"%h%y %T"` "BB [${LINENO}]  \t II = $II"
+CLOCK_PERIOD=$4
+echo -e `date +"%h%y %T"` "BB [${LINENO}]  \t CLOCK_PERIOD = $CLOCK_PERIOD"
+CLOCK_FREQ=$5
+echo -e `date +"%h%y %T"` "BB [${LINENO}]  \t CLOCK_FREQ = $CLOCK_FREQ"
+PARTID=$6
+echo -e `date +"%h%y %T"` "BB [${LINENO}]  \t PARTID = $PARTID"
+
+# setup Vivado
+. /home/drankin/setup_vivado.sh #setup vivado and vivado_hls (2018.1)
+#. /home/drankin/setup.sh.2016 #setup vivado and vivado_hls (2016.4)
+
+
+###################### HLS
+
+echo -e `date +"%h%y %T"` "BB [${LINENO}]  \t Preparing for HLS..."
+
+cp 
+
+SRC_HLS_DIR=${PF_DIR}firmware/data.h.bak
+DEST_HLS_DIR=${PF_DIR}firmware/data.h
+
+echo -e `date +"%h%y %T"` "BB [${LINENO}]  \t Source = $SRC_HLS_DIR"
+echo -e `date +"%h%y %T"` "BB [${LINENO}]  \t Destination = $DEST_HLS_DIR"
+
+if true; then #for debugging
+
+	cp $SRC_HLS_DIR $DEST_HLS_DIR
+
+	sed -i "s/NTRACK [0-9]*$/NTRACK ${NTRACKS}/g" $DEST_HLS_DIR
+	sed -i "s/NCALO [0-9]*$/NCALO ${NCALOS}/g" $DEST_HLS_DIR
+	sed -i "s/NEMCALO [0-9]*$/NEMCALO ${NCALOS}/g" $DEST_HLS_DIR
+	sed -i "s/NSELCALO [0-9]*$/NSELCALO `expr ${NCALOS} - 5`/g" $DEST_HLS_DIR
+	sed -i "s/MP7_NCHANN [0-9]*$/MP7_NCHANN `expr ${NTRACKS} + ${NTRACKS} + ${NCALOS} + ${NCALOS} + ${NCALOS} + ${NCALOS} + ${NCALOS} + ${NCALOS} + ${NCALOS} + ${NCALOS} - 10 + 4`/g" $DEST_HLS_DIR
+	
+	echo -e `date +"%h%y %T"` "BB [${LINENO}]  \t Running HLS..."
+	echo
+	
+        sed "s/II=[0-9]*/II=${II}/g" run_hls_fullpfalgo_w_puppi.tcl > run_hls_test.tcl
+        sed -i "s/create_clock -period [0-9\.]*/create_clock -period ${CLOCK_PERIOD}/g" run_hls_test.tcl
+        sed -i "s/set_part {.*}/set_part {${PARTID}}/g" run_hls_test.tcl
+
+	cd ${PF_DIR}
+        #rm -rf l1pf-resource-test
+	#vivado_hls -f run_hls_fullpfalgo_test.tcl
+        rm -rf l1pfpuppi-resource-test
+        #vivado_hls -f run_hls_fullpfalgo_w_puppi.tcl
+        vivado_hls -f run_hls_test.tcl
+
+	#cd ${PF_DIR}/puppi
+        #rm -rf proj_pfpuppi
+	#vivado_hls -f run_hls_pfpuppi.tcl
+
+	cd ${PF_DIR}
+	cp $SRC_HLS_DIR $DEST_HLS_DIR
+
+fi
+
+###################### Build vivado project
+if true; then #for debugging
+
+	echo -e `date +"%h%y %T"` "BB [${LINENO}]  \t Preparing for Vivado project..."
+
+	SRC_BUILD_DIR=$SCRIPT_DIR/build.tcl
+	#SRC_BUILD_DIR=$SCRIPT_DIR/build_forward.tcl
+	DEST_BUILD_DIR=$SCRIPT_DIR/modbuild.tcl
+
+	echo -e `date +"%h%y %T"` "BB [${LINENO}]  \t Source = $SRC_BUILD_DIR"
+	echo -e `date +"%h%y %T"` "BB [${LINENO}]  \t Destination = $DEST_BUILD_DIR"
+
+	cp $SRC_BUILD_DIR $DEST_BUILD_DIR
+
+#------CENTRAL
+	sed -i s/XX_NINPUTS_XX/$((2*(${NTRACKS} + (2 * ${NCALOS})) + 4))/g $DEST_BUILD_DIR
+	sed -i s/XX_NOUTPUTS_XX/$((2*(${NTRACKS} + (2 * ${NCALOS})) - 6))/g $DEST_BUILD_DIR
+	sed -i s/XX_NPFCH_XX/${NTRACKS}/g $DEST_BUILD_DIR
+	sed -i s/XX_NNEUTRAL_XX/$(((2 * ${NCALOS}) - 5))/g $DEST_BUILD_DIR
+
+#-------FORWARD
+#	sed -i s/XX_NINPUTS_XX/$((2*(${NTRACKS} + (2 * ${NCALOS})) + 4))/g $DEST_BUILD_DIR
+#	sed -i s/XX_NOUTPUTS_XX/$((2*(${NTRACKS} + (2 * ${NCALOS})) + 4))/g $DEST_BUILD_DIR
+#	sed -i s/XX_NPFCH_XX/${NTRACKS}/g $DEST_BUILD_DIR
+#	sed -i s/XX_NNEUTRAL_XX/$((2 * ${NCALOS}))/g $DEST_BUILD_DIR
+
+        sed -i s/XX_CLOCK_XX/${CLOCK_FREQ}/g $DEST_BUILD_DIR
+        sed -i s/XX_PART_XX/${PARTID}/g $DEST_BUILD_DIR
+
+	cd $SCRIPT_DIR
+
+	rm -rf pftest_tieoff vivado*.log vivado*.jou
+	vivado -mode batch -source $DEST_BUILD_DIR
+
+fi
+
