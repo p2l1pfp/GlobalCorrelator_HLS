f57bead79ede0f46b9ffe4c543e29337d14fecb5
Giovanni <gpetruc@gmail.com>
2020-10-30 10:24:49 +0100

    Add configs for 360MHz II=6 HGCal

---
+++ run_hls_pfalgo2hgc_2p5ns_II6.tcl
@@ -0,0 +1,35 @@
+# get the configuration
+set pfBoard "none"
+#set pfBoard "VCU118"
+set pfReg "HGCal"
+set cflags "-std=c++0x -DREG_${pfReg} -DBOARD_${pfBoard} -DHLS_pipeline_II=6"
+
+# open the project, don't forget to reset
+open_project -reset "proj_pf${pfReg}_${pfBoard}_2.5ns_II6_25"
+if { $pfBoard == "none" } {
+    set_top pfalgo2hgc
+} else {
+    set_top packed_pfalgo2hgc
+}
+add_files firmware/pfalgo2hgc.cpp -cflags "${cflags}"
+add_files -tb pfalgo2hgc_test.cpp  -cflags "${cflags}"
+add_files -tb ref/pfalgo2hgc_ref.cpp  -cflags "${cflags}"
+add_files -tb ref/pfalgo_common_ref.cpp  -cflags "${cflags}"
+add_files -tb utils/pattern_serializer.cpp -cflags "${cflags}"
+add_files -tb utils/test_utils.cpp -cflags "${cflags}"
+add_files -tb data/TTbar_PU200_HGCal.dump
+
+# reset the solution
+open_solution -reset "solution"
+set_part {xcvu9p-flga2104-2L-e}
+create_clock -period 2.5 -name default
+
+config_interface -trim_dangling_port
+# do stuff
+csim_design
+csynth_design
+#cosim_design -trace_level all
+#export_design -format ip_catalog -vendor "cern-cms" -version ${l1pfIPVersion} -description "${l1pfTopFunc}"
+
+# exit Vivado HLS
+exit
