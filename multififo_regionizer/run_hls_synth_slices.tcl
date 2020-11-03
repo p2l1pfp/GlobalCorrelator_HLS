8abbd3faa82c9aecd2b0166cb50fdf2d133c89ca
Giovanni <gpetruc@gmail.com>
2020-11-04 00:42:08 +0100

    TCL script for synthesis

---
+++ multififo_regionizer/run_hls_synth_slices.tcl
@@ -0,0 +1,37 @@
+source configIP.tcl
+
+set cflags "-std=c++0x -DREG_${pfReg} -DBOARD_${pfBoard} -DROUTER_NOMUX"
+set dets { "tk" "calo" }
+foreach det ${dets} {
+    if {${det} == "tk"} {
+        set slices { "input" "fifo" "merge2" "merge3" "output" }
+    } else {
+        set slices { "input" "fifo" "merge2" "merge4" "merge" "output" }
+    }
+
+    foreach slice ${slices} {
+        open_project -reset "project_synth_${det}_${slice}"
+        set hlsTopFunc ${det}_router_${slice}_slice
+        set_top ${hlsTopFunc}
+
+        add_files firmware/calo_regionizer.cpp -cflags "${cflags}"
+        add_files firmware/tk_regionizer.cpp -cflags "${cflags}"
+        add_files -tb regionizer_ref.cpp -cflags "${cflags}"
+        add_files -tb readMC.cpp -cflags "${cflags}"
+        add_files -tb regionizer_test.cpp -cflags "${cflags}"
+        add_files -tb ../utils/pattern_serializer.cpp -cflags "${cflags}"
+        add_files -tb ../utils/test_utils.cpp -cflags "${cflags}"
+        add_files -tb data/caloDump_hgcal.TTbar_PU200.txt
+        add_files -tb data/trackDump_hgcalPos.TTbar_PU200.txt
+
+        open_solution -reset "solution"
+        set_part {xcvu9p-flga2104-2L-e}
+        create_clock -period 2.5
+
+        csim_design
+        csynth_design
+        #cosim_design -trace_level all
+    }
+}
+
+exit
